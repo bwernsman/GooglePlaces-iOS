563568a09b30d55cb26aa8eb80654cac94610910
Ben Wernsman <ben.wernsman@me.com>
2016-10-18 14:35:18 -0500

    Initial Commit
    -Created Xcode project
    -Added Pods

---
+++ Google Places/Google Places/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Google Places
+//
+//  Created by Ben Wernsman on 10/18/16.
+//  Copyright © 2016 benw. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
